f02bb87f476fffb64aa578bac7a6d8f80657f3bd
aRTeeedgg <82109062+aRTeeedgg@users.noreply.github.com>
2023-11-15 13:17:00 -0500

    fixed typo that was breaking the whole thing and started working on database for shops Items

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, redirect, url_for, render_template
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 
-db = SQLALchemy(app)
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    discount = db.Column(db.Integer(), default=0)
+    stock = db.Column(db.Integer(), nullable=False)
+    desc = db.Column(db.String(length=150), nullable=False, unique=True)
+
+    category_id = db.Column(db.Integer(), db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category', backref='posts', lazy=True)
+
+    image_1 = db.Column(db.String(150), nullable=False, default='image.jpg')
+    image_2 = db.Column(db.String(150), nullable=False, default='image.jpg')
+    image_3 = db.Column(db.String(150), nullable=False, default='image.jpg')
+
+class Category(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=20), nullable=False, unique=True)
 
 @app.route("/")
 def home():
